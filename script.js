ffcd97c7daf288dc5546f1f899e7ec3695726d38
Prudence Lynne <prudencelynne56@gmail.com>
2023-11-10 10:31:05 +0300

    Update script.js and index.html

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let req=document.querySelector("#baddie");
-req.innerHTML="Mouse";
-let emojis=document.querySelector("#emoji");
-emojis.innerHTML="💇‍♀️💄";
+let req = document.querySelector("#baddie");
+req.innerHTML = "Mouse";
+let emojis = document.querySelector("#emoji");
+emojis.innerHTML = "💇‍♀️💄";
+function onClick(event) {
+  event.preventDefault();
+  event.target.innerHTML = "Thank you queen!😚😍";
+}
+let button = document.querySelector("button");
+button.addEventListener("click", onClick);
